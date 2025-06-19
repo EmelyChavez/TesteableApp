cf1741b02a6ce457be74d30ba12c75cd031d6e08
violetalisbeth <00007515@uca.edu.sv>
2025-06-19 11:41:25 -0600

    🐛:UnitTest Failed

---
+++ app/src/test/java/com/example/testeableapp/ExampleUnitTest3.kt
@@ -0,0 +1,14 @@
+package com.example.testeableapp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import com.example.testeableapp.ui.Screens.calculateTip
+
+class TipCalculatorUnitTest {
+
+    @Test
+    fun calcular_propina_con_monto_negativo() {
+        val resultado = calculateTip(-100.0, 15, false)
+        assertEquals(0.0, resultado, 0.0)
+    }
+}
