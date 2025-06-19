6c7440a5504fb6775f2c7896a6de156cb57053a5
KarlaMartinez1 <00021523@uca.edu.sv>
2025-06-19 11:23:36 -0600

    ✅ test Total a pagar por persona

--- app/src/test/java/com/example/testeableapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/testeableapp/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.testeableapp
 
+import com.example.testeableapp.ui.Screens.calculateTip
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -9,9 +10,20 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+
+class TipCalculatorUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun calcular_total_por_persona_correctamente() {
+        val montoCuenta = 100.0
+        val porcentajePropina = 20
+        val redondear = false
+        val personas = 4
+
+        val propinaEsperada = calculateTip(montoCuenta, porcentajePropina, redondear) // = 20.0
+        val totalEsperadoPorPersona = (montoCuenta + propinaEsperada) / personas     // = 30.0
+
+        assertEquals(20.0, propinaEsperada, 0.0)
+        assertEquals(30.0, totalEsperadoPorPersona, 0.0)
     }
 }
